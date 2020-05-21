33ff815e3f2ad54869c1986ed56df920061cc71c
Vanny Conoza <vanessa@smove.sg>
2020-05-21 20:40:53 +0800

    Make 'light' as default theme

--- src/context/theme.js
+++ src/context/theme.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 export const ThemeContext = React.createContext({
-  theme: '',
+  theme: 'light',
   setTheme: () => {},
 });
 
 const ThemeContextProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
+
   useEffect(() => {
     function loadTheme() {
       const theme = localStorage.getItem('theme');
       return theme || 'light';
     }
-    setTheme(loadTheme());
-  }, []);
-  useEffect(() => {
     localStorage.setItem('theme', theme);
+    setTheme(loadTheme());
   }, [theme]);
+
   return (
     <ThemeContext.Provider value={{ theme: theme, setTheme: setTheme }}>
       {children}
