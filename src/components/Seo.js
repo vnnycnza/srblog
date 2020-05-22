2394433acd930e0b548ced41b740b8c068223e43
Vanny Conoza <vanessa@smove.sg>
2020-05-23 07:07:17 +0800

    Fix twitter card

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -83,7 +83,7 @@ export default function Seo({
         },
       ]
         .concat(
-          metaImage
+          image
             ? [
                 {
                   property: 'og:image',
