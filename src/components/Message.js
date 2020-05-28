a4d55301555a1e6c7e0cb3fe98bedde236bfa747
Vanny Conoza <vanessa@smove.sg>
2020-05-28 18:29:50 +0800

    Fix 404 page & recent content

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Message({ content }) {
   return (
-    <section className="flex-row content-center">
+    <section className="flex-row content-center py-10 min-h-60-vh md:min-h-65-vh min-w-full">
       <h1 className="text-xl sm:text-2xl font-bold tracking-tighter leading-tight">
         {content}{' '}
         <span role="img" aria-label="Peace">
